43afa9ccddf1b2cc0f96a2c81dc3a25d4a85de94
RICHARD <quentin.richard@libertech.fr>
2020-10-09 14:59:38 +0200

    change

--- actions/info/change.js
+++ actions/info/change.js
@@ -1,1 +1,24 @@
+const inquirer = require("inquirer");
+const editJsonFile = require("edit-json-file");
+const path = require('path');
+
+let file = editJsonFile(path.resolve('./db.json'))
+
 "ajouter adresse de livraison"
+
+changeAdresseLivraison()
+
+function changeAdresseLivraison(){
+    inquirer.prompt([
+        {
+            name: "deliverAdress",
+            type: "input",
+            message:"Adresse de livraison"
+        }
+    ]).then(res => {
+        file.set("deliverAdress", res.deliverAdress)
+        file.save()
+    })
+}
+
+module.exports = {changeAdresseLivraison};
